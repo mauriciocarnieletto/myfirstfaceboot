81a323d2329f560446da66e3051445c17ba06767
Maurício Carnieletto <mauricio.carnieletto@gmail.com>
2017-01-09 17:35:18 -0200

    postback 4

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  sendMessage(senderID, {text: "Bem vindo bla bla bla"});
 }
 
 // Service start
