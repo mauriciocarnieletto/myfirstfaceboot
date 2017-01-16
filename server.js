3485dcec1ff88d4efc1317deaff882e587d1ec93
Maurício Carnieletto <mauricio.carnieletto@gmail.com>
2017-01-16 09:10:38 -0200

    Adding mongo and other libs

--- server.js
+++ server.js
@@ -1,48 +1,101 @@
 // Dependencies
-var express 	= require('express');
-var bodyParser 	= require('body-parser');
-var request 	= require('request');
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var request     = require('request');
+const NodeCache = require( "node-cache" );
 
 // Express
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// NodeCache
+const myCache = new NodeCache();
+
 // Server frontpage
 app.get('/', function (req, res) {
+
     res.send('<h1>This is TestBot Server!</h1> If you want to check it, access: <a href="https://www.facebook.com/testmyfirstfaceboot/">https://www.facebook.com/testmyfirstfaceboot/</a>');
 });
 
-// handler the facebook authentication
+/*
+ * Handler to the facebooks authentication
+ */
 app.get('/webhook', function (req, res) {
+
     if (req.query['hub.verify_token'] === 'EAADQ9m8UU5YBAMHitJLqnWCXepsxKgpyfMDOWUHclkkOFYUnZCFxbb6cXTyAAqWwRna0SrkqVhGHRrPo5xJ2O9zXZAQe7Qzt0gZCKVpooFAJgBOSZCEs4nmNbCuZCafpnWOs4yduLg9hRlR6utnGKbN2TuTmctglDZAkyC4SqnUAZDZD') {
-      res.send(req.query['hub.challenge']);
+
+        res.send(req.query['hub.challenge']);
+
     } else {
-      res.send('Error, wrong validation token');    
+
+        res.send('Error, wrong validation token');    
+
     }
 });
 
-// handler receiving messages
+
+/*
+ * Handler to recive message
+ */
 app.post('/webhook', function (req, res) {
+
     var events = req.body.entry[0].messaging;
+
     for (i = 0; i < events.length; i++) {
-        var event = events[i];
 
-        console.log(JSON.stringify(event));
+        var bot = {},
+            event = events[i];
 
-		if (event.message && event.message.text) {
-	    	firstMessage(event.sender.id, event.message.text);
-		} else if (event.postback) {
-            console.log('entrou');
-		    receivedPostback(event);
-		}
+        event.previousMessage = getPreviousMessage(event);
+        
+        storeMessage(event);
+
+        if (event.message && event.message.text) {
+
+            bot = require('./bot/welcome.js')(event);
+            
+            sendMessage(event.sender.id, botMessage['welcomeMenu'](event));
+
+        } else if (event.postback) {
+
+            var arr = event.postback.split('->');
+
+            bot = require('./bot/'+arr[0]+'.js')(event);
+            
+            sendMessage(event.sender.id, botMessage[arra[1]](event));
+        }
+        
+
     }
+
     res.sendStatus(200);
 });
 
+/*
+ *  StoreMessage
+ *  Stores the current message in memory
+ */
+function storeMessage(event) {
 
-// generic function sending messages
+    return myCache.set(event.sender.id, event.message);
+};
+
+/*
+ * getPreviousMessage
+ * Get previouse message from user
+ */
+function getPreviousMessage(event) {
+
+    return myCache.get(event.sender.id);
+};
+
+/*
+ * sendMessage   
+ * Get previouse message from user
+ */
 function sendMessage(recipientId, message) {
+
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
@@ -60,50 +113,12 @@ function sendMessage(recipientId, message) {
     });
 };
 
-// Send the first Message
-function firstMessage(recipientId, text) {
 
-    message = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Cândidato",
-                    "subtitle": "Candidato do Vestibular",
-                    "image_url": "https://image.shutterstock.com/display_pic_with_logo/2552089/236656255/stock-photo-optical-form-of-an-examination-236656255.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Candidato",
-                        "payload": "Candidato"
-                    }]
-                }, {
-                    "title": "Aluno Ensino Superior",
-                    "subtitle": "Aluno do Ensino Superior",
-                    "image_url": "https://image.shutterstock.com/display_pic_with_logo/162265/275161592/stock-photo-female-hands-with-pen-writing-on-notebook-275161592.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Aluno Ensino Superior",
-                        "payload": "Superior",
-                    }]
-                }, {
-                    "title": "Aluno Colégio",
-                    "subtitle": "Aluno do Colégio",
-                    "image_url": "https://image.shutterstock.com/display_pic_with_logo/1151480/301162325/stock-photo-back-to-school-background-with-rocket-made-from-pencils-301162325.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Aluno do Colégio",
-                        "payload": "Colegio",
-                    }]
-                }]
-            }
-        }
-    };
+// Service start
+app.listen((process.env.PORT || 3000));
 
-    sendMessage(recipientId, message);
-};
 
-function receivedPostback(event) {
+function receivePostback(event) {
 
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
@@ -112,14 +127,7 @@ function receivedPostback(event) {
   // The 'payload' param is a developer-defined field which is set in a postback 
   // button for Structured Messages. 
   var payload = event.postback.payload;
-
-  console.log("Received postback for user %d and page %d with payload '%s' " + 
-    "at %d", senderID, recipientID, payload, timeOfPostback);
-
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
   sendMessage(senderID, {text: "Bem vindo bla bla bla"});
 }
-
-// Service start
-app.listen((process.env.PORT || 3000));
