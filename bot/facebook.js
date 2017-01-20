d59b706d2f13441bb4aa0f54c7d5ff9bbf14da69
Maurício Carnieletto <mauricio.carnieletto@gmail.com>
2017-01-20 16:14:40 -0200

    db
    db

--- bot/facebook.js
+++ bot/facebook.js
@@ -33,21 +33,11 @@ function getPreviousEvent (event) {
  * Get previouse message from user
  */
 function sendMessage (recipientId, message, res) {
-    res.send(message);
-    console.log('---------------------message----------------------');
-    console.log(util.inspect(message));
-    console.log('---------------------inspect----------------------');
-    console.log(util.inspect({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: { id: recipientId },
-            message: message,
-        }
-    }));
-
-    // request({
+    // res.send(message);
+    // console.log('---------------------message----------------------');
+    // console.log(util.inspect(message));
+    // console.log('---------------------inspect----------------------');
+    // console.log(util.inspect({
     //     url: 'https://graph.facebook.com/v2.6/me/messages',
     //     qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
     //     method: 'POST',
@@ -55,13 +45,23 @@ function sendMessage (recipientId, message, res) {
     //         recipient: { id: recipientId },
     //         message: message,
     //     }
-    // }, function(error, response, body) {
-    //     if (error) {
-    //         console.log('Error sending message: ', error);
-    //     } else if (response.body.error) {
-    //         console.log('Error: ', response.body.error);
-    //     }
-    // });
+    // }));
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: { id: recipientId },
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
 };
 
 /**
